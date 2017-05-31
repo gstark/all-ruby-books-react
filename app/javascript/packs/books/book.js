8fe904374948e779443d2d27ab83db0438cce5fc
Gavin Stark <gavin@gstark.com>
2017-05-31 16:14:04 -0400

    removes unnecessary functions

--- app/javascript/packs/books/book.js
+++ app/javascript/packs/books/book.js
@@ -6,25 +6,15 @@ import './book.scss'
 
 export default class Book extends React.Component
 {
-  renderAuthors(authors) {
-    return authors.map(author => author.name).join(", ")
-  }
-
-  renderCategories(categories) {
-    return (
-      <ul className="categories">
-        {categories.map(category => <li key={category.name}>{category.name}</li>)}
-      </ul>
-    )
-  }
-
   render () {
     return (
       <Col md={4}>
         <div className="book-panel">
           <h2>{this.props.title}</h2>
-          {this.renderCategories(this.props.categories)}
-          <p className="authors">{this.renderAuthors(this.props.authors)}</p>
+          <ul className="categories">
+            {this.props.categories.map(category => <li key={category.name}>{category.name}</li>)}
+          </ul>
+          <p className="authors">{this.props.authors.map(author => author.name).join(", ")}</p>
           <p className="year">
             {this.props.editor}
             , {this.props.year}
