47fff5341dedf65809bcb7c63e2c662af64b14ef
Gavin Stark <gavin@gstark.com>
2017-05-31 13:17:57 -0400

    Adds database seeds
    rails db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+path = Rails.root.join("db/seeds/books", "*.json")
+Dir.glob(path) do |entry|
+  json = JSON.parse(File.read(entry))
+  authors = Array(json["authors"]).map { |author| Author.find_or_create_by(name: author) }
+  categories = Array(json["categories"]).map { |category| Category.find_or_create_by(name: category) }
+
+  Book.create(title: json["title"],
+              year: json["year"],
+              url: json["url"],
+              prices: json["prices"],
+              editor: json["editor"],
+              authors: authors,
+              categories: categories)
+end
