259bf40e7e1b635e25388224182988a33021aa54
Gavin Stark <gavin@gstark.com>
2017-05-31 13:55:49 -0400

    Bootstrap nav

--- app/javascript/packs/books/layout.js
+++ app/javascript/packs/books/layout.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import { Button } from 'react-bootstrap'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Button, Container } from 'react-bootstrap'
 
 export default class Layout extends React.Component
 {
   render () {
-    return <Button bsStyle='primary'>Click me!</Button>
+    return (
+     <Navbar>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <a href="/">All Ruby Books</a>
+          </Navbar.Brand>
+        </Navbar.Header>
+        <Nav>
+          <NavItem eventKey={1} href="/">Home</NavItem>
+        </Nav>
+      </Navbar>
+    )
   }
 }
 
