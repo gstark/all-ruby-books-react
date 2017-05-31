2fd6100376f17f34ee120252424b3bde2e03187e
Gavin Stark <gavin@gstark.com>
2017-05-31 14:02:20 -0400

    Creates a Books component

---
+++ app/javascript/packs/books/books.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Row, Col } from 'react-bootstrap'
+
+export default class Books extends React.Component
+{
+  render () {
+    return (
+      <Row>
+        <Col md={4}>
+          Bacon ipsum dolor amet t-bone sausage kielbasa, tongue ribeye pancetta boudin pork belly corned beef ground round prosciutto cupim flank pig tenderloin. Cupim pancetta salami pork belly meatball, alcatra biltong chicken tail turducken pig shoulder cow drumstick shankle. Drumstick t-bone kevin, cupim pork chop hamburger chuck. Ground round rump sirloin shankle short ribs alcatra t-bone, meatloaf bacon. Kielbasa short ribs pork tri-tip chuck cupim ham hock tenderloin ball tip doner jowl frankfurter ground round beef burgdoggen.
+        </Col>
+        <Col md={4}>
+          Bacon ipsum dolor amet t-bone sausage kielbasa, tongue ribeye pancetta boudin pork belly corned beef ground round prosciutto cupim flank pig tenderloin. Cupim pancetta salami pork belly meatball, alcatra biltong chicken tail turducken pig shoulder cow drumstick shankle. Drumstick t-bone kevin, cupim pork chop hamburger chuck. Ground round rump sirloin shankle short ribs alcatra t-bone, meatloaf bacon. Kielbasa short ribs pork tri-tip chuck cupim ham hock tenderloin ball tip doner jowl frankfurter ground round beef burgdoggen.
+        </Col>
+        <Col md={4}>
+          Bacon ipsum dolor amet t-bone sausage kielbasa, tongue ribeye pancetta boudin pork belly corned beef ground round prosciutto cupim flank pig tenderloin. Cupim pancetta salami pork belly meatball, alcatra biltong chicken tail turducken pig shoulder cow drumstick shankle. Drumstick t-bone kevin, cupim pork chop hamburger chuck. Ground round rump sirloin shankle short ribs alcatra t-bone, meatloaf bacon. Kielbasa short ribs pork tri-tip chuck cupim ham hock tenderloin ball tip doner jowl frankfurter ground round beef burgdoggen.
+        </Col>
+      </Row>
+    )
+  }
+}
+
--- app/javascript/packs/books/layout.js
+++ app/javascript/packs/books/layout.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Button, Container } from 'react-bootstrap'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Button, Grid } from 'react-bootstrap'
+
+import Books from './books'
 
 export default class Layout extends React.Component
 {
   render () {
     return (
-     <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="/">All Ruby Books</a>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          <NavItem eventKey={1} href="/">Home</NavItem>
-        </Nav>
-      </Navbar>
+      <div>
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">All Ruby Books</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="/">Home</NavItem>
+          </Nav>
+        </Navbar>
+        <Grid>
+          <Books/>
+        </Grid>
+      </div>
     )
   }
 }
